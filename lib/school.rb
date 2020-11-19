6be2d0bdedaded513371ffe04b229bcf30c8da9e
ionwolf13 <voli.scorp13@gmail.com>
2020-11-18 22:20:24 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+require 'pry'
+class School
+        attr_accessor :name, :roster
+        
+        def initialize(name)
+            @name = name
+            @roster = {}
+        end
+
+        def add_student(student,grade)
+            @roster[grade] ||= []
+            @roster[grade] << student
+            @roster
+        end
+        
+        def grade(grade)
+            @roster[grade]
+        end
+        binding.pry
+        def sort
+            @roster.sort.each do |key,value|
+                @roster[key] = @roster[key].sort
+            end
+            @roster
+        end
+end
+
+
+wharton = School.new("Wharton")
